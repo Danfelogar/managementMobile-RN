e39616c8979f52701d8d514d302a5792d164b4d9
Danfelogar <danypolo18@hotmail.com>
2022-12-12 22:38:34 -0500

    add conditional not found file

--- src/screens/inventory/Inventory.tsx
+++ src/screens/inventory/Inventory.tsx
@@ -1,5 +1,13 @@
 import React, {useContext} from 'react';
-import {View, Text, SafeAreaView, StatusBar, FlatList} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StatusBar,
+  FlatList,
+  ActivityIndicator,
+  Image,
+} from 'react-native';
 import IconFeather from 'react-native-vector-icons/Feather';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 
@@ -8,11 +16,12 @@ import {stylesInventory} from './stylesInventory';
 import {ModalFilter} from '../../components';
 import {InventoryContext, UIContext} from '../../context';
 import {InventoryCard} from './components';
+import {height, width} from '../../helpers';
 
 export const Inventory = () => {
   const {changeModalFilterInventory} = useContext(UIContext);
-  const {dataInventory} = useContext(InventoryContext);
-  const {background, secondary} = useInventory();
+  const {dataInventory, isLoading} = useContext(InventoryContext);
+  const {background, secondary, primary, textPrimary} = useInventory();
   // console.log({dataInventory});
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: secondary}}>
@@ -43,19 +52,65 @@ export const Inventory = () => {
           </Text>
         </View>
       </View>
-      <FlatList
-        data={dataInventory}
-        style={{
-          flex: 1,
-          backgroundColor: background,
-          paddingHorizontal: 20,
-          paddingTop: 13,
-        }}
-        contentContainerStyle={{paddingBottom: 98}}
-        keyExtractor={item => item._id}
-        renderItem={({item}) => <InventoryCard key={item._id} item={item} />}
-        // extraData={selectedId}
-      />
+      {isLoading || dataInventory.length === 0 ? (
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: background,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          {dataInventory.length === 0 && !isLoading && (
+            <>
+              <Image
+                style={{
+                  width: width / 1.4,
+                  height: height / 3,
+                  resizeMode: 'contain',
+                }}
+                source={{
+                  uri: 'https://cdn-icons-png.flaticon.com/512/1548/1548784.png',
+                }}
+              />
+              <Text
+                style={{
+                  ...stylesInventory.textTitleHeader,
+                  color: textPrimary,
+                }}>
+                Resultado no encontrados
+              </Text>
+            </>
+          )}
+          {isLoading && (
+            <>
+              <ActivityIndicator size="large" color={primary} />
+              <Text
+                style={{
+                  ...stylesInventory.textTitleHeader,
+                  color: textPrimary,
+                  marginTop: 10,
+                }}>
+                Cargando datos por favor espere...
+              </Text>
+            </>
+          )}
+        </View>
+      ) : (
+        <FlatList
+          data={dataInventory}
+          style={{
+            flex: 1,
+            backgroundColor: background,
+            paddingHorizontal: 20,
+            paddingTop: 13,
+          }}
+          contentContainerStyle={{paddingBottom: 98}}
+          keyExtractor={item => item._id}
+          renderItem={({item}) => <InventoryCard key={item._id} item={item} />}
+          // extraData={selectedId}
+        />
+      )}
+
       <ModalFilter />
     </SafeAreaView>
   );
